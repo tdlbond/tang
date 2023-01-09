c94ee7da3c74f4ec30f775390248929f84e048aa
tangdalu <tangdl134@163.com>
2023-01-09 10:41:44 +0800

    fix: eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
   },
   plugins: ['vue'],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off'
   }
 }
