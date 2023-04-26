057a5ef7d42e1bf06e01cb3049e15217150f0f39
tangdl <tangdl134@163.com>
2023-04-26 09:08:20 +0800

    fix: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
   overrides: [],
   parserOptions: {
     parser: '@babel/eslint-parser',
